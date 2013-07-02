326712e24eb984d2226ac0c76e50f5c2b52c98d7
Yasar icli <yasaricli@gmail.com>
2013-07-02 19:11:09 -0400

    auth application create account and profile models

--- game/apps/auth/models.py
+++ game/apps/auth/models.py
@@ -1,16 +1,34 @@
 # -*- coding: utf-8 -*- 
 
 from django.db import models
+from django.utils import timezone
 
+class Account(models.Model):
+    """
+        Users Model 
+    """
+    username = models.CharField("Username", max_length=30, unique=True)
+    password = models.CharField("password", max_length=128)
+    email = models.EmailField("Email", blank=True)
+    is_active = models.BooleanField("Active", default=True)
+    date_joined = models.DateTimeField("Date Joined", default=timezone.now)
+    last_login = models.DateTimeField("last login", default=timezone.now)
 
-#class User(models.Model):
-#
-#    username = models.CharField(max_length=30)
-#    password = models.CharField(max_length=20)
-#
-#    class Meta:
-#        app_label = "auth"
-#        db_table = "users"
-#
-#    def __unicode__(self):
-#        return u"%s" %self.username
+    class Meta:
+        app_label = "auth"
+        db_table = "account"
+
+    def __unicode__(self):
+        return u"%s" %(self.username)
+
+class Profile(models.Model):
+    account = models.ForeignKey(Account)
+    first_name = models.CharField("First Name", max_length=30, blank=True)
+    last_name = models.CharField("Last Name", max_length=30, blank=True)
+
+    class Meta:
+        app_label = "auth"
+        db_table = "profile"
+
+    def __unicode__(self):
+        return u"%s" %(self.first_name)
