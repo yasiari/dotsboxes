84d0cd6498c35891d02dc82a52afe76a0132caa9
Yaşar içli <yasaricli@gmail.com>
2013-07-03 14:07:35 +0300

    forms auth file

--- game/apps/auth/__init__.py
+++ game/apps/auth/__init__.py
@@ -5,22 +5,17 @@ from apps.auth.models import User
 
 
 def authenticate(username=None, password=None):
-
     """
         User check password return User 
-
         authenticate(username=username, password=password) 
     """
-
     try:
         user = User.objects.get(username=username, is_active=True)
     except:
         user = None
-   
     else: 
         if user and user.check_password(password):
             return user
-
     return None
 
 
